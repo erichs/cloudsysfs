075d71598cb1fd668ab041ecaf25f92f64215314
Erich Smith <heartquest@gmail.com>
2017-04-12 22:24:35 -0400

    Add doc

--- detect.go
+++ detect.go
@@ -1,10 +1,9 @@
+// Package cloudsysfs provides a function for detecting the current host's cloud provider, based on the contents of the /sys filesystem.
 package cloudsysfs
 
-import (
-	"github.com/erichs/cloudsysfs/providers"
-)
+import "github.com/erichs/cloudsysfs/providers"
 
-var Providers = [...]func(chan<- string){
+var cloudProviders = [...]func(chan<- string){
 	providers.AWS,
 	providers.Azure,
 	providers.DigitalOcean,
@@ -12,14 +11,16 @@ var Providers = [...]func(chan<- string){
 	providers.OpenStack,
 }
 
+// Detect tries to detect the current cloud provider a host is using.
+// It returns a lowercase string identifying the provider if found, or empty string if none were detected.
 func Detect() string {
 	sysfscheck := make(chan string)
-	for _, cloud := range Providers {
+	for _, cloud := range cloudProviders {
 		go cloud(sysfscheck)
 	}
 
 	provider := ""
-	for i := 0; i < len(Providers); i++ {
+	for i := 0; i < len(cloudProviders); i++ {
 		v := <-sysfscheck
 		if v != "" {
 			provider = v
